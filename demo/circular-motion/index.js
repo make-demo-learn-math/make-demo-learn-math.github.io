e185b4b89a4b391ff6d0abf5b75bc799cf8f85c2
Ken Whatmough <ken.whatmough@bluescape.com>
2021-02-07 12:47:03 -0500

    thoughts

--- demo/circular-motion/index.js
+++ demo/circular-motion/index.js
@@ -1,8 +1,19 @@
-import Platform from "../../src/Platform.js";
-import App from "./App.js";
+import DemoContext from "../../src/DemoContext.js";
+import CircularMotionDemo from "./CircularMotionDemo.js";
 
 const MAIN_CANVAS_ID = "main-canvas"; // by convention
 
-new Platform().run(MAIN_CANVAS_ID, (context) => {
-  new App({ context }).start();
-});
+let theDemoContext;
+
+try {
+  theDemoContext = new DemoContext(MAIN_CANVAS_ID);
+  new CircularMotionDemo().start(theDemoContext);
+} catch (err) {
+  if (theDemoContext) {
+    theDemoContext.close();
+  }
+
+  const mainWindow = window;
+  mainWindow.console.error({ err }, "An unexpected error occurred");
+  mainWindow.alert(`Unexpected error: ${err.message}`);
+}
--- src/DemoContext.js
+++ src/DemoContext.js
@@ -6,14 +6,15 @@ import Ui from "./Ui.js";
 export default class DemoContext {
   constructor(
     canvasId,
+    featureNames = [],
     deps = { Features, Pseudorandom, Timer, Ui, window: globalThis }
   ) {
     const canvas = deps.window.document.getElementById(canvasId);
+    this.ui = new deps.Ui(canvas);
 
-    this.features = new deps.Features();
+    this.features = new deps.Features(featureNames);
     this.pseudorandom = new deps.Pseudorandom();
     this.timer = new deps.Timer();
-    this.ui = new deps.Ui(canvas);
     this.visual = canvas.getContext("2d");
 
     // Automatically resize the canvas
