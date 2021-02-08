e31d8d3afb140e26c045c137b70e92f3b7f1d015
Ken Whatmough <ken.whatmough@bluescape.com>
2021-02-08 06:46:54 -0500

    improve error case

--- src/DemoContext.js
+++ src/DemoContext.js
@@ -10,11 +10,14 @@ export default class DemoContext {
     deps = { Features, Pseudorandom, Timer, Ui, window: globalThis }
   ) {
     const canvas = deps.window.document.getElementById(canvasId);
-    this.ui = new deps.Ui(canvas);
+    if (!canvas) {
+      throw new Error(`No element found with ID ${canvasId}`);
+    }
 
     this.features = new deps.Features(featureNames);
     this.pseudorandom = new deps.Pseudorandom();
     this.timer = new deps.Timer();
+    this.ui = new deps.Ui(canvas);
     this.visual = canvas.getContext("2d");
 
     // Automatically resize the canvas
